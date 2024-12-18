ce7cad5af4695f7999f1c12ec6a0cbbf014999f5
Pierluigi <pigi.ped@hotmail.it>
2024-12-18 16:49:20 +0100

    collision parte2

--- LearnCAgain/main.c
+++ LearnCAgain/main.c
@@ -4,11 +4,17 @@
 #include <SDL_image.h>
 #include <time.h>
 
+#define GRAVITY 0.35f
+
 typedef struct
 {
-	int x, y;
+	float x, y;
+	float dx, dy;
 	short life;
 	char* name;
+	int onLedge;
+
+	int animFrame, facingRight, slowingDown;
 } Man;
 
 typedef struct {
@@ -34,6 +40,8 @@ typedef struct
 	SDL_Texture* crystal;
 	SDL_Texture* menFrames[2];
 	SDL_Texture* brick;
+
+	int time;
 
 	// Renderer
 	SDL_Renderer* renderer;
@@ -90,6 +98,14 @@ void loadGame(GameState *game)
 
 	game->man.x = 320 - 40;
 	game->man.y = 240 - 40;
+	game->man.dx = 0;
+	game->man.dy = 0;
+	game->man.onLedge = 0;
+	game->man.animFrame = 0;
+	game->man.facingRight = 0;
+	game->man.slowingDown = 0;
+
+	game->time = 0;
 
 	// init crystals
 	for (int i = 0; i < 100; i++)
@@ -108,12 +124,43 @@ void loadGame(GameState *game)
 	}
 	game->ledges[99].x = 350;
 	game->ledges[99].y = 200;
+
+	game->ledges[98].x = 350;
+	game->ledges[98].y = 350;
 }
 
 //useful utility function to see if two rectangles are colliding at all
 int collide2d(float x1, float y1, float x2, float y2, float wt1, float ht1, float wt2, float ht2)
 {
 	return(!((x1 > (x2 + wt2)) || (x2 > (x1 + wt1)) || (y1 > (y2 + ht2)) || (y2 > (y1 + ht1))));
+}
+
+void process(GameState* game)
+{
+	//add time
+	game->time++;
+
+	//man movement
+	Man* man = &game->man;
+	man->x += man->dx;
+	man->y += man->dy;
+
+	if (man->dx != 0 && man->onLedge && !man->slowingDown)
+	{
+		if (game->time % 10 == 0)
+		{
+			if (man->animFrame == 0)
+			{
+				man->animFrame = 1;
+			}
+			else
+			{
+				man->animFrame = 0;
+			}
+		}
+	}
+
+	man->dy += GRAVITY;
 }
 
 void collisionDetect(GameState* game)
@@ -125,22 +172,55 @@ void collisionDetect(GameState* game)
 		float mx = game->man.x, my = game->man.y;
 		float bx = game->ledges[i].x, by = game->ledges[i].y, bw = game->ledges[i].w, bh = game->ledges[i].h;
 
-		if (my < by+bh && my+mh > by)
+		if (mx+mw /2 > bx && mx+mw /2 < bx+bw)
 		{
-			// tocco sul lato destro
-			if (mx < bx+bw && mx+mw > bx+bw)
+			// controllo se sbatte la capoccia
+			if (my < by+bh && my > by && game->man.dy < 0)
+			{
+				//correggo y
+				game->man.y = by+bh;
+				my = by+bh;
+
+				// fermo il salto
+				game->man.dy = 0;
+				game->man.onLedge = 1;
+			}
+		}
+		//controllo se intersecano sull'asse x
+		if (mx+mw > bx && mx < bx+bw)
+		{
+			// controllo se sto per terra
+			if (my+mh > by && my < by && game->man.dy > 0)
+			{
+				//correggo y
+				game->man.y = by-mh;
+				my = by-mh;
+
+				// fermo la caduta
+				game->man.dy = 0;
+				game->man.onLedge = 1;
+			}
+		}
+		//controllo se intersecano sull'asse y
+		if (my+mh > by && my<by+bh)
+		{
+			// m interseca a sinistra, quindi tocca il muro a sx
+			if (mx < bx+bw && mx+mw > bx+bw && game->man.dx < 0)
 			{
 				// correggo x
 				game->man.x = bx+bw;
 				mx = bx+bw;
+
+				game->man.dx = 0;
 			}
-			else if (mx+mw > bx && mx < bx)
+			else if (mx+mw > bx && mx < bx && game->man.dx > 0) // m interseca a destra, quindi tocca il muro a dx
 			{
 				// correggo x
 				game->man.x = bx-mw;
 				mx = bx-mx;
-			}
 
+				game->man.dx = 0;
+			}
 		}
 	}
 }
@@ -171,6 +251,12 @@ int processEvents(SDL_Window* window, GameState* game)
 			case SDLK_ESCAPE:
 				done = 1;
 				break;
+			case SDLK_UP:
+				if (game->man.onLedge)
+				{
+					game->man.dy = -8;
+					game->man.onLedge = 0;
+				}
 			}
 		}
 		break;
@@ -180,23 +266,52 @@ int processEvents(SDL_Window* window, GameState* game)
 		}
 	}
 
+
+	//More jumping
 	const Uint8* state = SDL_GetKeyboardState(NULL);
-	if (state[SDL_SCANCODE_LEFT])
-	{
-		game->man.x -= 10;
-	}
-	if (state[SDL_SCANCODE_RIGHT])
-	{
-		game->man.x += 10;
-	}
 	if (state[SDL_SCANCODE_UP])
 	{
-		game->man.y -= 10;
+		game->man.dy -= 0.2f;
 	}
-	if (state[SDL_SCANCODE_DOWN])
+
+	//walking
+	if (state[SDL_SCANCODE_LEFT])
 	{
-		game->man.y += 10;
+		game->man.dx -= 0.5;
+		if (game->man.dx < -6)
+		{
+			game->man.dx = -6;
+		}
+		game->man.facingRight = 0;
+		game->man.slowingDown = 0;
 	}
+	else if (state[SDL_SCANCODE_RIGHT])
+	{
+		game->man.dx += 0.5;
+		if (game->man.dx > 6)
+		{
+			game->man.dx = 6;
+		}
+		game->man.facingRight = 1;
+		game->man.slowingDown = 0;
+	}
+	else {
+		game->man.animFrame = 0;
+		game->man.dx *= 0.8f;
+		game->man.slowingDown = 1;
+		if (fabsf(game->man.dx) < 0.1f)
+		{
+			game->man.dx = 0;
+		}
+	}
+	//if (state[SDL_SCANCODE_UP])
+	//{
+	//	game->man.y -= 10;
+	//}
+	//if (state[SDL_SCANCODE_DOWN])
+	//{
+	//	game->man.y += 10;
+	//}
 	return done;
 }
 
@@ -214,13 +329,13 @@ void doRender(SDL_Renderer* renderer, GameState* game)
 	//draw the ledges
 	for (int i = 0; i < 100; i++)
 	{
-		SDL_Rect ledgeRect = { game->ledges[i].w,game->ledges[i].h,game->ledges[i].x,game->ledges[i].y };
+		SDL_Rect ledgeRect = { game->ledges[i].x,game->ledges[i].y,game->ledges[i].w,game->ledges[i].h };
 		SDL_RenderCopy(renderer, game->brick, NULL, &ledgeRect);
 	}
 
 	// draw rectangle at man position
 	SDL_Rect rect = { game->man.x,game->man.y,48,48 };
-	SDL_RenderCopyEx(renderer, game->menFrames[0], NULL, &rect, 0, NULL, 0);
+	SDL_RenderCopyEx(renderer, game->menFrames[game->man.animFrame], NULL, &rect, 0, NULL, game->man.facingRight);
 
 	//draw the crystal image
 	//for (int i = 0; i < 100; i++)
@@ -265,6 +380,7 @@ int main(int argc, char* args[]) {
 		// Check for events
 		done = processEvents(window, &gameState);
 
+		process(&gameState);
 		collisionDetect(&gameState);
 
 		// Render display
